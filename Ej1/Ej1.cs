eef813455686170e2c4721102ff50bac6b1e4f56
Federico Eliceche <Fede.eliceche@gmail.com>
2024-05-23 17:47:49 -0300

    Add files via upload

---
+++ Ej1/Ej1.cs
@@ -0,0 +1,25 @@
+﻿﻿using System;
+
+namespace Ej1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("App para realizar el promedio de 3 numeros, presiona enter para continuar... ");
+            Console.ReadLine();
+            Console.WriteLine("Ingrese el primer número:");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo número:");
+            double n2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el tercer número:");
+            double n3 = Convert.ToDouble(Console.ReadLine());
+
+            double promedio = (n1 + n2 + n3) / 3;
+
+            Console.WriteLine("El promedio es: " + promedio);
+        }
+    }
+}
